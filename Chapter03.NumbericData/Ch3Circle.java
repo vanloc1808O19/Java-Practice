a5504fbe09ab58563df75c6a1f920808821cddcc
vanloc1808 <vanloc1808@gmail.com>
2022-03-07 10:21:33 +0700

    chapter 3 circle

---
+++ Chapter03.NumbericData/Ch3Circle.java
@@ -0,0 +1,22 @@
+/*
+    Chapter 3 Sample Program: compute area and circumference of a circle
+*/
+
+import javax.swing.*;
+import java.text.*;
+
+class Ch3Circle {
+    public static void main(String[] args) {
+        final double PI = 3.14159; // constant
+
+        String radiusStr = JOptionPane.showInputDialog(null, "Enter radius: ");
+        double radius = Double.parseDouble(radiusStr);
+
+        double area = PI * radius * radius;
+        double circumference = 2 * PI * radius;
+
+        JOptionPane.showMessageDialog(null, "Given radius: " + radius + "\n");
+        JOptionPane.showMessageDialog(null, "Area: " + area + "\n");
+        JOptionPane.showMessageDialog(null, "Circumference: " + circumference + "\n");
+    }
+}
