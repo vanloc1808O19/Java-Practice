1a7d75828d940fb7d3f3b0e28113eba68d495402
vanloc1808 <vanloc1808@gmail.com>
2022-03-12 18:15:40 +0700

    chap 13 graduate student

---
+++ Chapter12.InheritanceAndPolymorphism/Ch13GraduateStudent.java
@@ -0,0 +1,15 @@
+public class Ch13GraduateStudent extends Ch13Student {
+    public void computeCourseGrade() {
+        int total = 0;
+
+        for (int i = 0; i < NUM_OF_TESTS; i++) {
+            total += test[i];
+        }
+
+        if (total / NUM_OF_TESTS >= 80) {
+            courseGrade = "Pass";
+        } else {
+            courseGrade = "Fail";
+        }
+    }    
+}
